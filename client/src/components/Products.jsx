3d05cd1150a071fadcb7070575d413397472f0c5
Federico Robledo <federicorobledo85@gmail.com>
2023-10-10 17:43:15 -0300

    ready to deploy

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { SideMenu } from "./SideMenu";
+import { Rating } from "primereact/rating";
 
 export const Products = () => {
   const [products, setProducts] = useState([]);
@@ -39,27 +40,29 @@ export const Products = () => {
           setProducts={setProducts}
         />
         <div>
-          <div className=" text-white flex flex-col md:grid grid-cols-2 lg:grid-cols-3 ">
+          <div className=" text-white flex flex-col md:grid grid-cols-2 xl:grid-cols-3 ">
             {close ? (
               <div>
                 <div>
                   {description.map((item) => {
                     return (
                       <>
-                        <div className=" absolute z-10 items-center w-[30%] ml-[10%] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-16 mb-10 p-4">
+                        <div className=" absolute z-10 items-center w-[30%] ml-[10%] mt-24 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-16 mb-10 p-4">
                           <img
                             className="block mx-auto max-h-72 rounded-md rounded-bl-none rounded-br-none"
                             src={item.img_Url}
                             alt="product image"
                           />
-                          <div className="text-center space-y-3 md:text-left flex flex-col">
+                          <div className="text-center space-y-6 md:text-left flex flex-col">
                             <p className="text-gray-500 font-medium">
-                              <span className="normal-case text-white">
+                              <span className="normal-case text-white my-10">
                                 {item.description}
                               </span>
                             </p>
+                            <span>${item.price}</span>
+                            <Rating value={5} readOnly cancel={false} />
                           </div>
-                          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mx-4">
+                          <button className="text-white mt-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mx-4">
                             Add to cart
                           </button>
                           <button
@@ -81,20 +84,21 @@ export const Products = () => {
             {products.map((product) => {
               return (
                 <>
-                  <div className=" py-6 px-6 max-w-md mr-40 bg-white rounded-xl shadow-md space-y-2 my-8">
+                  <div className="py-6 px-6 max-w-md mx-auto bg-white rounded-xl shadow-md space-y-2 my-8">
                     <img
                       className="block mx-auto max-h-72 rounded-md rounded-bl-none rounded-br-none"
                       src={product.img_Url}
-                      alt="Construction work"
+                      alt="racquet"
                     />
-                    <div className="text-center space-y-3 md:text-left flex flex-col">
-                      <p className="text-lg text-black font-semibold">
-                        <a href="#" title="Visite site">
-                          <span className="capitalize text-black">
-                            {product.name}
-                          </span>
-                        </a>
-                      </p>
+                    <div className="text-left space-y-3 flex items-center">
+                      <div className="space-y-0.5 flex-grow">
+                        <p className="text-lg text-black font-semibold">
+                          <span>{product.name}</span>
+                        </p>
+                        <p className="text-black font-medium">
+                          <span>{product.price}</span>
+                        </p>
+                      </div>
                     </div>
                     <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mx-4">
                       Add to cart
--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -5,7 +5,7 @@ export const SideMenu = ({
   products,
 }) => {
   return (
-    <div className=" bg-gradient-to-r from-gray-400 via-gray-500 to-gray-700 flex md:flex-col p-8 text-center md:w-[30%]  ">
+    <div className=" bg-gradient-to-r from-gray-400 via-gray-500 to-gray-700 flex-wrap md:flex-col p-8 text-center md:w-[30%]  ">
       {menuItems.map((val) => (
         <button
           onClick={() => filterProducts(val)}
@@ -17,7 +17,7 @@ export const SideMenu = ({
       ))}
       <button
         onClick={() => setProducts(products)}
-        className="inline-block rounded-md border border-transparent dark:bg-gray-800 dark:border-gray-700 px-8 py-3 text-center font-medium text-white my-8 lg:w-[60%]"
+        className="inline-block rounded-md border border-transparent dark:bg-gray-800 dark:border-gray-700 px-8 py-1 text-center font-medium text-white my-8 lg:w-[60%]"
       >
         All
       </button>
